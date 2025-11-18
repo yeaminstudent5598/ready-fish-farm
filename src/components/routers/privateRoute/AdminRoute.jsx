bd8bc669ff511695550b6621f84c84e874e98f63
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-11-18 20:32:33 +0600

    Update: src/components/routers/privateRoute/AdminRoute.jsx

---
+++ src/components/routers/privateRoute/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import useAuth from '../../../Hooks/useAuth';
+import useAdmin from '../../../Hooks/useAdmin';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const AdminRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
+        return <div className="flex justify-center items-center min-h-screen">Loading...</div>;
+    }
+
+    if (user && isAdmin) {
+        return children;
+    }
+
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default AdminRoute;
